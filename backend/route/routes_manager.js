332e25dccdb857375d50e1b1f1dbaa530cf577db
Chanon Jenakom <chanonjenakom@gmail.com>
2017-04-30 12:16:38 +0700

    Render

--- backend/route/routes_manager.js
+++ backend/route/routes_manager.js
@@ -3,17 +3,50 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'Store'
+  database: 'gamestore_database'
 });
+
 module.exports = function (app) {
-  
-  ////////////////////
-  // default route  //
-  ////////////////////
+
+  ////////////////
+  // test route //
+  ////////////////
+  app.route('/test')
+    .get((req, res) => {
+      connection.query('SELECT name, price FROM games WHERE price < 1000', (err, result) => {
+        return res.json(result)
+      })
+    })
+
+  ////////////////
+  // home route //
+  ////////////////
   app.route('/')
     .get((req, res) => {
-      return res.status(200).json({
-        message: 'Hello World! yahoo'
-      })
+      res.render('index.html')
+    })
+
+  ////////////////////////
+  // registration route //
+  ////////////////////////
+  app.route('/regis')
+    .get((req, res) => {
+      res.render('registration.html')
+    })
+
+  /////////////////
+  // login route //
+  /////////////////
+  app.route('/login')
+    .get((req, res) => {
+      res.render('login.html')
+    })
+
+  /////////////////
+  // store route //
+  /////////////////
+  app.route('/store')
+    .get((req, res) => {
+      res.render('store.html')
     })
 }
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
+const path = require('path')
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, 'views')))
 app.use(bodyParser.json());
 app.use(cors())
 app.listen(3000);
 console.log('Store@ http://localhost:' + 3000);
-
-app.route('/test')
-  .get((req, res) => {
-    return res.status(200).json({
-      message: 'Hello World!'
-    })
-  })
 
 require('./route/routes_manager.js')(app);
