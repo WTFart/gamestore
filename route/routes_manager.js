d681e2b2569ddbf7b9afc087899b2b58a0eb2924
Chanon Jenakom <chanonjenakom@gmail.com>
2017-05-07 14:21:00 +0700

    Store search functionality

--- route/routes_manager.js
+++ route/routes_manager.js
@@ -31,11 +31,19 @@ module.exports = function (app, passport) {
           /////////////////
           // store route //
           /////////////////
-          connection.query('SELECT game_id, name, price FROM games WHERE except_country NOT IN (SELECT country FROM stores WHERE store_id = ?) AND game_id NOT IN (SELECT game_id FROM orders WHERE user_id = ?)',
-          [req.cookies.store_id, req.cookies.user_id],
-          (err, result) => {
-            res.render('store.ejs', { user_id: req.cookies.user_id, games: result })
-          })
+          if (req.query.search) {
+            connection.query('SELECT game_id, name, price FROM games WHERE except_country NOT IN (SELECT country FROM stores WHERE store_id = ?) AND game_id NOT IN (SELECT game_id FROM orders WHERE user_id = ?) AND name LIKE ?',
+            [req.cookies.store_id, req.cookies.user_id, '%' + req.query.search + '%'],
+            (err, result) => {
+              res.render('store.ejs', { user_id: req.cookies.user_id, games: result })
+            })
+          } else {
+            connection.query('SELECT game_id, name, price FROM games WHERE except_country NOT IN (SELECT country FROM stores WHERE store_id = ?) AND game_id NOT IN (SELECT game_id FROM orders WHERE user_id = ?)',
+            [req.cookies.store_id, req.cookies.user_id],
+            (err, result) => {
+              res.render('store.ejs', { user_id: req.cookies.user_id, games: result })
+            })
+          }
         } else if (path == '/library') {
           ///////////////////
           // library route //
