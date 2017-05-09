7c82ea73461b081bada46eef3f213a9a9f517a12
babestvl <babestvl@gmail.com>
2017-05-09 09:17:50 +0700

    Pull merge

---
+++ views/js/store.js
@@ -0,0 +1,93 @@
+/* Search bar */
+
+var resizeElements;
+
+$(document).ready(function() {
+
+  // Set up common variables
+  // --------------------------------------------------
+
+  var bar = ".search_bar";
+  var input = bar + " input[type='text']";
+  var button = bar + " button[type='submit']";
+  var dropdown = bar + " .search_dropdown";
+  var dropdownLabel = dropdown + " > span";
+  var dropdownList = dropdown + " ul";
+  var dropdownListItems = dropdownList + " li";
+
+
+  // Set up common functions
+  // --------------------------------------------------
+
+  resizeElements = function() {
+    var barWidth = $(bar).outerWidth();
+
+    var labelWidth = $(dropdownLabel).outerWidth();
+    $(dropdown).width(labelWidth);
+
+    var dropdownWidth = $(dropdown).outerWidth();
+    var buttonWidth	= $(button).outerWidth();
+    var inputWidth = barWidth - dropdownWidth - buttonWidth;
+    var inputWidthPercent = inputWidth / barWidth * 100 + "%";
+
+    $(input).css({ 'margin-left': dropdownWidth, 'width': inputWidthPercent });
+  }
+
+  function dropdownOn() {
+    $(dropdownList).fadeIn(25);
+    $(dropdown).addClass("active");
+  }
+
+  function dropdownOff() {
+    $(dropdownList).fadeOut(25);
+    $(dropdown).removeClass("active");
+  }
+
+
+  // Initialize initial resize of initial elements 
+  // --------------------------------------------------
+  resizeElements();
+
+
+  // Toggle new dropdown menu on click
+  // --------------------------------------------------
+
+  $(dropdown).click(function(event) {
+    if ($(dropdown).hasClass("active")) {
+      dropdownOff();
+    } else {
+      dropdownOn();
+    }
+
+    event.stopPropagation();
+    return false;
+  });
+
+  $("html").click(dropdownOff);
+
+
+  // Activate new dropdown option and show tray if applicable
+  // --------------------------------------------------
+
+  $(dropdownListItems).click(function() {
+    $(this).siblings("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+
+    // Focus the input
+    $(this).parents("form.search_bar:first").find("input[type=text]").focus();
+
+    var labelText = $(this).text();
+    $(dropdownLabel).text(labelText);
+
+    resizeElements();
+
+  });
+
+
+  // Resize all elements when the window resizes
+  // --------------------------------------------------
+
+  $(window).resize(function() {
+    resizeElements();
+  });
+});
