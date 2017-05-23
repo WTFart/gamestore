44e9444ea94fcb71d5febd7b5e104753b98ef0f0
Chanon Jenakom <chanonjenakom@gmail.com>
2017-05-23 15:05:40 +0700

    Update database

--- gamestore_database.sql
+++ gamestore_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 14, 2017 at 07:49 PM
+-- Generation Time: May 23, 2017 at 10:03 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -79,27 +79,27 @@ CREATE TABLE `games` (
 --
 
 INSERT INTO `games` (`game_id`, `name`, `developer`, `publisher`, `genre`, `price`, `review`, `age_limit`, `release_date`, `except_country`, `description`) VALUES
-(1, 'Dead by daylight', 1, 1, '{Horror, Survival, Horror, Multiplayer, Co-op}', 369, 4, 18, '14-Jun-2016', '', 'Dead by Daylight is a multiplayer (4vs1) horror game where one player takes on the role of the savage Killer, and the other four players play as Survivors, trying to escape the Killer and avoid being caught and killed.'),
-(2, 'Grand Theft Auto V', 2, 2, '{Open World, Action, Multiplayer, First-Person}', 1499, 5, 18, '14-Apr-2015', '', 'Strap on a Stunt Suit and get ready for 20 electrifying new Stunt Races custom built for the Rocket Voltic, Ruiner 2000 & Blazer Aqua, in GTA Online Cunning Stunts: Special Vehicle Circuit. Also, Legendary Motorsport adds the Progen GP1 to its formidable roster of supercars.'),
-(3, 'Counter-Strike: Global Offensive', 3, 3, '{FPS, Multiplayer, Shooter, Action, Team-Based}', 315, 5, 13, '22-Aug-2012', '', 'Counter-Strike: Global Offensive (CS: GO) will expand upon the team-based action gameplay that it pioneered when it was launched 14 years ago. CS: GO features new maps, characters, and weapons and delivers updated versions of the classic CS content (de_dust, etc.).'),
+(1, 'Dead by daylight', 1, 1, '{Horror, Survival, Horror, Multiplayer, Co-op}', 369, 4, 18, '14-Jun-2016', 'China', 'Dead by Daylight is a multiplayer (4vs1) horror game where one player takes on the role of the savage Killer, and the other four players play as Survivors, trying to escape the Killer and avoid being caught and killed.'),
+(2, 'Grand Theft Auto V', 2, 2, '{Open World, Action, Multiplayer, First-Person}', 1499, 5, 18, '14-Apr-2015', 'China', 'Strap on a Stunt Suit and get ready for 20 electrifying new Stunt Races custom built for the Rocket Voltic, Ruiner 2000 & Blazer Aqua, in GTA Online Cunning Stunts: Special Vehicle Circuit. Also, Legendary Motorsport adds the Progen GP1 to its formidable roster of supercars.'),
+(3, 'Counter-Strike: Global Offensive', 3, 3, '{FPS, Multiplayer, Shooter, Action, Team-Based}', 315, 5, 13, '22-Aug-2012', 'China', 'Counter-Strike: Global Offensive (CS: GO) will expand upon the team-based action gameplay that it pioneered when it was launched 14 years ago. CS: GO features new maps, characters, and weapons and delivers updated versions of the classic CS content (de_dust, etc.).'),
 (4, 'Hearthstone', 4, 4, '{Collectible card game}', 0, 4, 7, '11-Mar-2014', '', 'Hearthstone is a digital-only collectible card game that revolves around turn-based matches between two opponents (player vs. player or player vs. computer opponent) using pre-made decks of cards from their collection.'),
-(5, 'Overwatch', 4, 4, '{First-person shooter}', 1560, 5, 12, '24-May-2016', '', 'Overwatch is a first-person shooter that features squad-based combat with two opposing teams of six players each.[1] Players choose one of several hero characters, each with their own unique abilities and role classes.'),
-(6, 'Diablo 3', 4, 4, '{Action, RPG, hack and slash}', 1250, 4, 16, '15-May-2012', '', 'Diablo 3 is an action role-playing hack and slash video game. The player moves and interacts with the environment primarily by way of a mouse.[7] Other actions, such as casting a spell, are performed in response to keyboard inputs.[7] The player can acquire items, learn spells, defeat enemies, and interact with non-player characters (NPC)s throughout the game.'),
-(7, 'Nier:Automata', 6, 5, '{Action, RPG, hack and slash}', 1990, 5, 16, '23-Feb-2017', '', 'Nier: Automata is an open-world action role-playing game set on a post-apocalyptic Earth thousands of years after the events of the original Nier.[1] The player takes the role of combat androids from the YoRHa units across an open world environment.'),
+(5, 'Overwatch', 4, 4, '{First-person shooter}', 1560, 5, 12, '24-May-2016', 'Hong Kong', 'Overwatch is a first-person shooter that features squad-based combat with two opposing teams of six players each.[1] Players choose one of several hero characters, each with their own unique abilities and role classes.'),
+(6, 'Diablo 3', 4, 4, '{Action, RPG, hack and slash}', 1250, 4, 16, '15-May-2012', 'India', 'Diablo 3 is an action role-playing hack and slash video game. The player moves and interacts with the environment primarily by way of a mouse.[7] Other actions, such as casting a spell, are performed in response to keyboard inputs.[7] The player can acquire items, learn spells, defeat enemies, and interact with non-player characters (NPC)s throughout the game.'),
+(7, 'Nier:Automata', 6, 5, '{Action, RPG, hack and slash}', 1990, 5, 16, '23-Feb-2017', 'U.K.', 'Nier: Automata is an open-world action role-playing game set on a post-apocalyptic Earth thousands of years after the events of the original Nier.[1] The player takes the role of combat androids from the YoRHa units across an open world environment.'),
 (8, 'Final Fantasy XV', 5, 5, '{Action, RPG}', 2190, 5, 16, '29-Nov-2016', '', 'Final Fantasy XV takes place on the fictional world of Eos. All the world\'s countries, bar the kingdom of Lucis, are under the dominion of the empire of Niflheim. Noctis Lucis Caelum, heir to the Lucian throne, goes on a quest to retake his homeland and its magical Crystal after it is seized by Niflheim on the eve of peace negotiations between the two nations. However, he soon discovers that the Crystal is at the heart of a much greater threat to Eos, and that his role as the future king is key to averting an apocalyptic event from taking place.'),
-(9, 'Persona 5', 7, 6, '{RPG, social simulation}', 1990, 5, 16, '4-Apr-2017', '', 'Persona 5 takes place in Tokyo, and follows a silent protagonist after their transfer to the fictional Shujin Academy after being put on probation for an assault he was falsely accused of. During the course of a school year, he and other students awaken to their Persona powers, become masked vigilantes dubbed the \"Phantom Thieves of Hearts\", and explore the supernatural Palace realm to steal ill intent from the hearts of adults. As with previous titles in the series, the party does battle with supernatural enemies using manifestations of their psyche called Personas. The game incorporates role-playing elements and dungeon exploration alongside social simulation scenarios.'),
-(10, 'Guild wars 2', 8, 7, '{MMORPG}', 1850, 4, 12, '28-Aug-2012', '', 'Guild Wars 2 claims to be unique in the genre by featuring a storyline that is responsive to player actions, something which is common in single player role-playing games but rarely seen in multiplayer ones. A dynamic event system replaces traditional questing, utilising the ripple effect to allow players to approach quests in different ways as part of a persistent world. Also of note is the combat system, which aims to be more dynamic than its predecessor by promoting synergy between professions and using the environment as a weapon, as well as reducing the complexity of the Magic-style skill system of the original game.'),
+(9, 'Persona 5', 7, 6, '{RPG, social simulation}', 1990, 5, 16, '4-Apr-2017', 'U.K.', 'Persona 5 takes place in Tokyo, and follows a silent protagonist after their transfer to the fictional Shujin Academy after being put on probation for an assault he was falsely accused of. During the course of a school year, he and other students awaken to their Persona powers, become masked vigilantes dubbed the \"Phantom Thieves of Hearts\", and explore the supernatural Palace realm to steal ill intent from the hearts of adults. As with previous titles in the series, the party does battle with supernatural enemies using manifestations of their psyche called Personas. The game incorporates role-playing elements and dungeon exploration alongside social simulation scenarios.'),
+(10, 'Guild wars 2', 8, 7, '{MMORPG}', 1850, 4, 12, '28-Aug-2012', 'Spain', 'Guild Wars 2 claims to be unique in the genre by featuring a storyline that is responsive to player actions, something which is common in single player role-playing games but rarely seen in multiplayer ones. A dynamic event system replaces traditional questing, utilising the ripple effect to allow players to approach quests in different ways as part of a persistent world. Also of note is the combat system, which aims to be more dynamic than its predecessor by promoting synergy between professions and using the environment as a weapon, as well as reducing the complexity of the Magic-style skill system of the original game.'),
 (11, 'Limbo', 9, 8, '{Adventure, Indie}', 219, 5, 16, '21-Jul-2010', '', 'The primary character in Limbo is a nameless boy, who awakens in the middle of a forest on the \"edge of hell\" (the game\'s title is taken from the Latin limbus, meaning \"edge\").While seeking his missing sister, he encounters only a few human characters who either attack him, run away, or are dead. At one point during his journey, he encounters a female character, but is prevented from reaching her. The forest eventually gives way to a crumbling city environment. On completion of the final puzzle, the boy is thrown through a pane of glass and back into the forest. He walks a short distance until he again encounters a girl, who, upon his approach, stands up, startled. At this point, the game abruptly ends.'),
 (12, 'Transistor', 10, 9, '{Action, Indie, RPG}', 369, 5, 12, '20-May-2014', '', 'Red, a famous singer in a city called Cloudbank, is attacked by the Process, a robotic force commanded by a group called the Camerata. She manages to escape and comes into possession of the mysterious Transistor—the great sword-like weapon she was to be assassinated with. The Transistor is buried into the chest of a man, now slumped over and dead; though his consciousness and voice seems to have been absorbed into the Transistor itself along with Red\'s voice. The Camerata continue to track Red and the Transistor down with the Process, wanting the weapon for some yet-unknown cause.'),
-(13, 'Bastion', 10, 9, '{Action, Indie, RPG}', 315, 5, 12, '20-Jul-2011', '', 'The game takes place in the aftermath of the Calamity, a catastrophic event that suddenly fractured the city of Caelondia (/seɪˈlɒndiə/) as well as the surrounding areas of the game\'s world into many floating pieces, disrupting its ecology and reducing most of its people to ash. Players take control of the Kid, a silent protagonist who awakens on one of the few remaining pieces of the old world and sets off for the eponymous Bastion, where everyone was supposed to go in troubled times.'),
+(13, 'Bastion', 10, 9, '{Action, Indie, RPG}', 315, 5, 12, '20-Jul-2011', 'Japan', 'The game takes place in the aftermath of the Calamity, a catastrophic event that suddenly fractured the city of Caelondia (/seɪˈlɒndiə/) as well as the surrounding areas of the game\'s world into many floating pieces, disrupting its ecology and reducing most of its people to ash. Players take control of the Kid, a silent protagonist who awakens on one of the few remaining pieces of the old world and sets off for the eponymous Bastion, where everyone was supposed to go in troubled times.'),
 (14, 'Risk of Rain', 11, 10, '{Action, Indie, RPG}', 219, 4, 7, '8-Nov-2013', '', 'The player controls the survivor of a space freighter crash on a strange planet. As the player progresses through levels, selected randomly and with some procedural placement of objects within the level, they attempt to survive by killing monsters and collecting items that can boost their offensive and defensive abilities. The game features a difficulty scale that increases with time, requiring the player to choose between spending time building experience and completing levels quickly before the monsters become more difficult. By discovering various hidden locations, players can discover artifacts which can alter gameplay. The game supports up to four cooperative players in online play and up to two players in local play..'),
 (15, 'Dark Soul 3', 12, 11, '{Action, RPG}', 1500, 4, 16, '12-Apr-2016', '', 'Set in the Kingdom of Lothric, a bell has rung to signal that the First Flame, responsible for prolonging the Age of Fire, is dying out. As has happened many times before, the coming of the Age of Dark produces the undead, cursed beings that rise up after death. The Age of Fire can be prolonged with the linking of the fires, a ritual in which great lords and heroes sacrifice their souls to kindle the First Flame. However, Prince Lothric has abandoned his duty and instead chooses to watch the flame die from afar. The bell is the last hope for the Age of Fire, raising previous Lords of Cinder, heroes who have linked the flame across aeons to attempt to link the fire again. However, all but one Lord abandon their thrones rather than linking the fire again.'),
-(16, 'INSIDE', 9, 8, '{Atmospheric, Dark, Indie, Adventure, Puzzle}', 369, 5, 18, '7-Jul-2016', '', 'Hunted and alone, a boy finds himself drawn into the center of a dark project.'),
+(16, 'INSIDE', 9, 8, '{Atmospheric, Dark, Indie, Adventure, Puzzle}', 369, 5, 18, '7-Jul-2016', 'India', 'Hunted and alone, a boy finds himself drawn into the center of a dark project.'),
 (17, 'Warhammer 40,000: Dawn of War III', 13, 12, '{Strategy, RTS, Sci-fi, Action}', 1129, 0, 16, '28-Apr-2017', '', 'Step into a brutal battle between three warring factions In Dawn of War III you will have no choice but to face your foes when a catastrophic weapon is found on the mysterious world of Acheron.'),
 (18, 'Project CARS', 14, 11, '{Racing, Simulation, Driving, Realistic, Sports}', 955, 4, 3, '16-Mar-2015', '', 'Project CARS is the ultimate driver journey!'),
-(19, 'Portal', 3, 3, '{Puzzle, First-Person, Singleplayer, Sci-fi, Action}', 219, 5, 0, '10-Oct-2007', '', 'Portal™ is a new single player game from Valve. Set in the mysterious Aperture Science Laboratories, Portal has been called one of the most innovative new games on the horizon and will offer gamers hours of unique gameplay.'),
+(19, 'Portal', 3, 3, '{Puzzle, First-Person, Singleplayer, Sci-fi, Action}', 219, 5, 0, '10-Oct-2007', 'Hong Kong', 'Portal™ is a new single player game from Valve. Set in the mysterious Aperture Science Laboratories, Portal has been called one of the most innovative new games on the horizon and will offer gamers hours of unique gameplay.'),
 (20, 'Batman™: Arkham Knight', 15, 13, '{Batman, Action, OpenWorld, Superhero, Dark}', 369, 4, 18, '23-Jun-2015', '', 'Batman™: Arkham Knight brings the award-winning Arkham trilogy from Rocksteady Studios to its epic conclusion. Developed exclusively for New-Gen platforms, Batman: Arkham Knight introduces Rocksteady\'s uniquely designed version of the Batmobile.'),
-(21, 'Outlast 2', 16, 14, '{Psychological Horror, FirstPerson, Nudity, Violent}', 599, 4, 18, '25-Apr-2017', '', 'Outlast 2 introduces you to Sullivan Knoth and his followers, who left our wicked world behind to give birth to Temple Gate, a town, deep in the wilderness and hidden from civilization. Knoth and his flock are preparing for the tribulations of the end of times and you’re right in the thick of it.');
+(21, 'Outlast 2', 16, 14, '{Psychological Horror, FirstPerson, Nudity, Violent}', 599, 4, 18, '25-Apr-2017', 'Singapore', 'Outlast 2 introduces you to Sullivan Knoth and his followers, who left our wicked world behind to give birth to Temple Gate, a town, deep in the wilderness and hidden from civilization. Knoth and his flock are preparing for the tribulations of the end of times and you’re right in the thick of it.');
 
 -- --------------------------------------------------------
 
@@ -150,9 +150,7 @@ INSERT INTO `orders` (`order_id`, `user_id`, `game_id`, `price`, `payment_id`, `
 (27, 12, 20, 369, 9, '2017-02-26 17:12:28'),
 (28, 25, 19, 219, 12, '2017-02-26 21:27:18'),
 (29, 26, 4, 0, 15, '2017-02-27 08:58:41'),
-(30, 29, 10, 1850, 13, '2017-02-28 10:54:10'),
-(31, 2, 2, 1499, 8, '2017-05-14 13:09:27'),
-(32, 2, 3, 315, 8, '2017-05-14 14:03:10');
+(30, 29, 10, 1850, 13, '2017-02-28 10:54:10');
 
 -- --------------------------------------------------------
 
@@ -330,7 +328,7 @@ CREATE TABLE `wishlists` (
   `wish_id` int(2) NOT NULL,
   `user_id` int(2) DEFAULT NULL,
   `game_id` int(2) DEFAULT NULL,
-  `date` datetime DEFAULT NULL
+  `date` datetime(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
@@ -401,12 +399,12 @@ ALTER TABLE `wishlists`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `order_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `payment_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -416,7 +414,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `wishlists`
 --
 ALTER TABLE `wishlists`
-  MODIFY `wish_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `wish_id` int(2) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
